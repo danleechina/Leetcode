1de84ebf6a6ac4ecd8abdf07695cc0e1dd95bbbc
ampire_dan <smile47777@gmail.com>
2018-08-21 11:08:55 +0800

    solve q3

--- 3.cpp
+++ 3.cpp
@@ -1,22 +1,40 @@
 #include <string>
 #include <vector>
+#include <set>
 #include <iostream>
 
 class Solution {
 public:
     int lengthOfLongestSubstring(std::string s) {
-      std::vector<int> info(52, -1);
+      std::set<char> info;
       int maxLength = 0;
+      int start = 0;
+      int end = 0;
       for(int index = 0; index + s.begin() != s.end(); index ++) {
-        auto ch = *(index + s.begin());
-        if (ch >= 'a' && ch <= 'z') {
-          
+        auto element = *(index + s.begin());
+        auto it = info.find(element);
+        if (it != info.end()) {
+          maxLength = std::max(maxLength, end - start);
+          auto tempStartElement = *(start + s.begin());
+          while (tempStartElement != element) {
+            info.erase(info.find(tempStartElement));
+            start ++;
+            tempStartElement = *(start + s.begin());
+          }
+          info.erase(it);
+          info.insert(element);
+          start ++;
+        } else {
+          info.insert(element);
         }
+        end = index + 1;
       }
-      return maxLength;
+      return std::max(maxLength, end - start);
     }
 };
 
 int main() {
+  auto sln = Solution();
+  std::cout<< sln.lengthOfLongestSubstring("pwwkew") << '\n';
   return 0;
 }
