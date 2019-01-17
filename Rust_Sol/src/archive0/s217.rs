149d5d10987c483a7998750f1f92b9c1361575e5
danleechina <349604757@qq.com>
2019-01-17 09:46:52 +0800

    solve 217 using Rust

--- Rust_Sol/src/archive0/s217.rs
+++ Rust_Sol/src/archive0/s217.rs
@@ -1,3 +1,12 @@
 pub fn print_hello() {
     println!("Hello, world!");
 }
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+      let len = nums.len();
+      let a: HashSet<_> = nums.into_iter().collect();
+      a.len() == nums.len()
+    }
+}
