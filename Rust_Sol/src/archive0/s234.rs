19c05c6b821227522d509726adeaf394a98faa15
danleechina <349604757@qq.com>
2019-02-11 13:53:04 +0800

    solve 234 using Rust

--- Rust_Sol/src/archive0/s234.rs
+++ Rust_Sol/src/archive0/s234.rs
@@ -5,67 +5,42 @@ pub struct ListNode {
   pub next: Option<Box<ListNode>>
 }
 
-pub struct Solution{}
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
 
-impl ListNode {
-  #[inline]
-  fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
-    }
-  }
-}
-impl ListNode {
+struct Solution{}
 
-  pub fn printNode(&self) {
-    let mut x: &ListNode = self;
-    println!("{}", x.val);
-    while let Some(ref nx) = x.next {
-      x = nx;
-      println!("{}", x.val);
-    }
-  }
-
-  pub fn len(&self) -> i32 {
-    let mut tmp: &ListNode = self;
-    let mut res = 1;
-    while let Some(ref t) = tmp.next {
-      res += 1;
-      tmp = t;
-    }
-    res
-  }
-
-  pub fn revese(&mut self) -> &Self {
-    return self;
-  }
-
-}
 impl Solution {
-  pub fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
-    if None == head {
+    pub fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
+      if (head == None) { return true; }
+      let mut vec = Vec::new();
+      let mut current = head;
+      while let Some(node) = current {
+        vec.push(node.val);
+        current = node.next;
+      }
+      let mut back = vec.len()/2;
+      let mut front = vec.len()/2 - 1;
+      if vec.len() % 2 == 1 {
+        front = vec.len()/2;
+      }
+      while front >= 0 {
+        if vec[front] != vec[back] {
+          return false;
+        }
+        if front == 0 {
+            return true;
+        }
+        front -= 1;
+        back += 1;
+      }
       return true;
     }
-    let len = head.unwrap().len();
-    println!("{}", len);
-    let mut half = (len + 1) / 2;
-    let mut tail = &head.unwrap();
-    while half > 0 {
-      tail = &tail.next.unwrap();
-      half -= 1;
-    }
-    tail = tail.revese();
-    half = (len + 1) / 2;
-    let mut h = &head.unwrap();
-    while half > 0 {
-      if tail.val != h.val {
-        return false;
-      } else {
-        tail = &tail.next.unwrap();
-        h = &h.next.unwrap();
-      }
-    }
-    return true;
-  }
 }
