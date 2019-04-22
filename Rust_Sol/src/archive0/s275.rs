14ebdb94433567b7fb7fef89fbe6e9e4aa1ceafc
danleechina <349604757@qq.com>
2019-04-22 10:06:40 +0800

    solve s275 using Rust

---
+++ Rust_Sol/src/archive0/s275.rs
@@ -0,0 +1,26 @@
+
+impl Solution {
+  pub fn h_index(citations: Vec<i32>) -> i32 {
+    let mut res = 0;
+    // let mut citations = citations;
+    // citations.sort();
+    let len = citations.len();
+    for (index, item) in citations.iter().enumerate() {
+      let item = *item as usize;
+      if item == 0 {
+        continue;
+      }
+      if index + item - 1 < len {
+        if citations[index + item - 1] >= item as i32 {
+          res = item;
+        }
+      } else {
+        if len - index <= item && res < len - index {
+          res = len - index;
+        }
+        return res as i32;
+      }
+    }
+    return res as i32;
+  }
+}
